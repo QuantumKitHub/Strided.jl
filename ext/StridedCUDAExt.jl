6814a52dd73e85d35a248ad45f64c817fb92d421
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-02-26 10:15:41 -0500

    Add Number restriction

--- ext/StridedCUDAExt.jl
+++ ext/StridedCUDAExt.jl
@@ -6,7 +6,7 @@ using CUDA: GPUArrays
 
 const ALL_FS = Union{typeof(adjoint), typeof(conj), typeof(identity), typeof(transpose)}
 
-function Base.copy!(dst::StridedView{TD, ND, TAD, FD}, src::StridedView{TS, NS, TAS, FS}) where {TD, ND, TAD <: CuArray{TD}, FD <: ALL_FS, TS, NS, TAS <: CuArray{TS}, FS <: ALL_FS}
+function Base.copy!(dst::StridedView{TD, ND, TAD, FD}, src::StridedView{TS, NS, TAS, FS}) where {TD <: Number, ND, TAD <: CuArray{TD}, FD <: ALL_FS, TS <: Number, NS, TAS <: CuArray{TS}, FS <: ALL_FS}
     bc_style = Base.Broadcast.BroadcastStyle(TAS)
     bc = Base.Broadcast.Broadcasted(bc_style, identity, (src,), axes(dst))
     GPUArrays._copyto!(dst, bc)
