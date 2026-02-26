3326f5a9aa9bd2a37901d57795062b8529c6fd33
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-02-26 05:20:22 -0500

    Workaround for size zero

--- test/amd.jl
+++ test/amd.jl
@@ -1,7 +1,11 @@
 for T in (Float32, Float64, Complex{Float32}, Complex{Float64})
     @testset "Copy with ROCStridedView: $T, $f1, $f2" for f2 in (identity, conj, adjoint, transpose), f1 in (identity, conj, transpose, adjoint)
         for m1 in (0, 16, 32), m2 in (0, 16, 32)
-            A1 = AMDGPU.randn(T, (m1, m2))
+            if iszero(m1 * m2)
+                A1 = AMDGPU.ROCMatrix{T}(undef, (m1, m2))
+            else
+                A1 = AMDGPU.randn(T, (m1, m2))
+            end
             A2 = similar(A1)
             A1c = copy(A1)
             A2c = copy(A2)
