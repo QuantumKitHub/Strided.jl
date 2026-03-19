ab912b48c3c452ba39fe67a25ba5ba9bb3c1bd18
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-03-19 07:20:15 -0400

    Remove unneeded copyto

--- ext/StridedGPUArraysExt.jl
+++ ext/StridedGPUArraysExt.jl
@@ -42,30 +42,6 @@ function Base.fill!(A::StridedView{T, N, TA, F}, x) where {T, N, TA <: AbstractG
     return A
 end
 
-# kernel-based variant for copying between wrapped GPU arrays
-@kernel function linear_copy_kernel!(dest, dstart, src, sstart, n)
-    i = @index(Global, Linear)
-    if i <= n
-        @inbounds dest[dstart + i - 1] = src[sstart + i - 1]
-    end
-end
-
-function Base.copyto!(
-        dest::StridedView{TD, ND, TAD, FD}, dstart::Integer,
-        src::StridedView{TS, NS, TAS, FS}, sstart::Integer, n::Integer
-    ) where {TD, TS, ND, NS, TAD <: AbstractGPUArray{TD}, TAS <: AbstractGPUArray{TS}, FD, FS}
-    n == 0 && return dest
-    n < 0 && throw(ArgumentError(string("tried to copy n=", n, " elements, but n should be nonnegative")))
-    destinds, srcinds = LinearIndices(dest), LinearIndices(src)
-    (checkbounds(Bool, destinds, dstart) && checkbounds(Bool, destinds, dstart + n - 1)) || throw(BoundsError(dest, dstart:(dstart + n - 1)))
-    (checkbounds(Bool, srcinds, sstart)  && checkbounds(Bool, srcinds, sstart + n - 1))  || throw(BoundsError(src, sstart:(sstart + n - 1)))
-    kernel = linear_copy_kernel!(KernelAbstractions.get_backend(dest))
-    kernel(dest, dstart, src, sstart, n; ndrange = n)
-    return dest
-end
-Base.copyto!(dest::StridedView{TD, ND, TAD, FD}, src::StridedView{TS, NS, TAS, FS}) where {TD, TS, ND, NS, TAD <: AbstractGPUArray{TD}, TAS <: AbstractGPUArray{TS}, FD, FS} = copyto!(dest, 1, src, 1, length(src))
-
-
 function LinearAlgebra.mul!(
         C::StridedView{TC, 2, <:AnyGPUArray{TC}},
         A::StridedView{TA, 2, <:AnyGPUArray{TA}},
