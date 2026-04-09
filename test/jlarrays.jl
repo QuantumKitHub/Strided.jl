d7c2affd6cee1bb1fc8f51c16efc07c55f6fc6a2
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-04-09 02:31:01 -0400

    Better test

--- test/jlarrays.jl
+++ test/jlarrays.jl
@@ -12,8 +12,11 @@
             axes(f1(A1)) == axes(f2(A2)) || continue
             @test collect(Matrix(copy!(f2(A2), f1(A1)))) == JLArrays.Adapt.adapt(Vector{T}, copy!(B2, B1))
             @test copy!(zA1, f1(A1)) == copy!(zA2, B1)
-            B1 .= 2 .* B1 .- B1 ./ 3 # test copyto! of Broadcasted
-            A1 .= 2 .* A1 .- A1 ./ 3 # test copyto! of Broadcasted
+            A3 = JLArray(randn(T, (m1, m2)))
+            A3c = copy(A3)
+            B3 = f1(StridedView(A3c))
+            @. B1 = 2 * B1 - B3 / 3 # test copyto! of Broadcasted
+            @. A1 = 2 * A1 - A3 / 3 # test copyto! of Broadcasted
             @test JLArrays.Adapt.adapt(Vector{T}, f1(A1)) == JLArrays.Adapt.adapt(Vector{T}, B1)
             x = rand(T)
             @test f1(StridedView(JLArrays.Adapt.adapt(Vector{T}, fill!(A1c, x)))) == JLArrays.Adapt.adapt(Vector{T}, fill!(B1, x))
